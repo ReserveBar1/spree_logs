f7fa7b82205d78479c1f47038caa6b32469b39cb
Thomas Boltze <thomas.boltze@gmail.com>
2012-12-31 15:39:05 +0800

    Added pagination and a bit of error handling

--- app/controllers/spree/admin/log_entries_controller.rb
+++ app/controllers/spree/admin/log_entries_controller.rb
@@ -4,7 +4,21 @@ module Spree
       respond_to :html
       #before_filter :load_data
       
-    
+      def index
+        respond_with(@collection)
+      end
+      
+      protected
+	
+    	def collection
+        return @collection if @collection.present?
+
+        @search = LogEntry.metasearch(params[:search])
+        @collection = @search.relation.page(params[:page]).per(Spree::Config[:admin_products_per_page])
+ 
+      end
+    	
+      
     end
   end
 end
